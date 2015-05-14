7ba2dcd4534dbfbaf61aed0599a18b089b0e5409
Eduardo Moreno <eduardo@Eduardos-MacBook-Pro.local>
2015-05-13 17:44:59 -0700

    adds flashing random functionality

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ function makeBoard(){
 	for (var i = 0; i < 9; i++){ //row
 
 		for (var j = 0; j < 9; j++){ //col
-
+			
+			var rand1 = Math.floor(Math.random()*256).toString();
+			var rand2 = Math.floor(Math.random()*256).toString();
+			var rand3 = Math.floor(Math.random()*256).toString();
+	
 			var aSquare = document.createElement("div");
 			aSquare.style.width = "11.1%";
 			aSquare.style.float = "left";
 			aSquare.style.paddingBottom = "11.1%";
 			if (flag)
-				aSquare.style.backgroundColor = "black";
+				aSquare.style.backgroundColor = "rgb("+rand1+","+rand2+","+rand3+")";
 			else
-				aSquare.style.backgroundColor = "red";
+				aSquare.style.backgroundColor = "rgb("+rand1+","+rand2+","+rand3+")";
 			body.appendChild(aSquare);
 			flag = !flag
 		}
@@ -22,3 +26,23 @@ function makeBoard(){
 }
 
 makeBoard();
+
+function changeColor(){
+	var divs = document.querySelectorAll("div");
+	for (var i = 0; i < 81; i++){
+
+		var rand1 = Math.floor(Math.random()*256).toString();
+		var rand2 = Math.floor(Math.random()*256).toString();
+		var rand3 = Math.floor(Math.random()*256).toString();
+
+		divs[i].style.backgroundColor = "rgb("+rand1+","+rand2+","+rand3+")";
+	}
+}
+
+function continuousChange(){
+	for(var i = 0; i < 200; i++){
+		window.setTimeout(changeColor,250);
+	}
+}
+for(var i = 0; i < 2000; i++)
+	continuousChange();
