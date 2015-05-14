0fe40cef25d7b451262867064241ac71959ac452
Eduardo Moreno <eduardo@Eduardos-MacBook-Pro.local>
2015-05-13 17:01:26 -0700

    initial commit: r and b

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // Your JS goes here
+
+function makeBoard(){
+	var body = document.querySelector("body");
+	var flag = true;
+	for (var i = 0; i < 9; i++){ //row
+
+		for (var j = 0; j < 9; j++){ //col
+
+			var aSquare = document.createElement("div");
+			aSquare.style.width = "11.1%";
+			aSquare.style.float = "left";
+			aSquare.style.paddingBottom = "11.1%";
+			if (flag)
+				aSquare.style.backgroundColor = "black";
+			else
+				aSquare.style.backgroundColor = "red";
+			body.appendChild(aSquare);
+			flag = !flag
+		}
+	}
+}
+
+makeBoard();
