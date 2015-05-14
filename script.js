99ed364fe81db65062cea3abeb7b6495cf6e0e65
Eduardo Moreno <eduardo@Eduardos-MacBook-Pro.local>
2015-05-13 17:30:40 -0700

    adds gradient functionality

--- script.js
+++ script.js
@@ -3,7 +3,12 @@
 function makeBoard(){
 	var body = document.querySelector("body");
 	var flag = true;
+	var rand = 65;
+	var rand1 = 180;
 	for (var i = 0; i < 9; i++){ //row
+
+		rand1 -= 6;
+		rand += 6;
 
 		for (var j = 0; j < 9; j++){ //col
 
@@ -11,10 +16,11 @@ function makeBoard(){
 			aSquare.style.width = "11.1%";
 			aSquare.style.float = "left";
 			aSquare.style.paddingBottom = "11.1%";
-			if (flag)
-				aSquare.style.backgroundColor = "black";
-			else
-				aSquare.style.backgroundColor = "red";
+			if (flag) {
+				aSquare.style.backgroundColor = "rgb("+rand+","+rand1+","+rand+")";
+			}else{
+				aSquare.style.backgroundColor = "rgb("+rand1+","+rand+","+rand1+")";
+			}
 			body.appendChild(aSquare);
 			flag = !flag
 		}
